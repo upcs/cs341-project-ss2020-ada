87ebf7dfd54009c2792610bc3e908e31645732fc
dendinge21 <dendinge21@up.edu>
2020-02-18 18:17:50 -0800

    filter button menu
    Created a button that when clicked displays a side panel that will have the options for the filters that can be added on the map when the apply button is clicked.

--- PERU/public/javascripts/index_script.js
+++ PERU/public/javascripts/index_script.js
@@ -1,3 +1,10 @@
 function change_page(value) {
     window.location.href = value;
   }
+  function openNav() {
+    document.getElementById("mySidepanel").style.width = "250px";
+  }
+  
+  function closeNav() {
+    document.getElementById("mySidepanel").style.width = "0";
+  }
