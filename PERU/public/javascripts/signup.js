089dfdd29b3aaa86f2c2a1d40c06997a8b979bdb
dendinge21 <dendinge21@up.edu>
2020-02-20 11:04:02 -0800

    post from signup.js

--- PERU/public/javascripts/signup.js
+++ PERU/public/javascripts/signup.js
@@ -4,3 +4,15 @@ const form   = document.querySelector('.form')
 button.addEventListener('click', function() {
    form.classList.add('form--no') 
 });
+
+$(".btn").click(function() {
+   /*External Citation: https://www.w3schools.com/jquery/jquery_dom_set.asp */
+       var user = $("#user").text();
+       var email = $("#email").text();
+       var password = $("password").text();
+       /*Processing the response*/
+       $.post("/newUsers",{ usrname:user,  usrpw:password, loc1name: 0, loc1x:0, loc1y: 0, loc2name: 0, loc2x:0, loc2y: 0, user_email: email },
+            function(data){
+               
+           }, "json"); 
+    });
