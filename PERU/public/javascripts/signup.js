b78f20d1d6070bf8813eeb5b5417b271c7ca39b1
slemly <slemly@outlook.com>
2020-02-25 11:05:12 -0800

    Cases of searching for existing users. CURRENTLTY NOT WORKING

--- PERU/public/javascripts/signup.js
+++ PERU/public/javascripts/signup.js
@@ -1,28 +1,29 @@
-$('.btn').click(function(event){
-   event.preventDefault();
-   //External Citation: https://www.w3schools.com/jquery/jquery_dom_set.asp
-   var user = $('#user').val();
-   var email = $('#email').val();
-   var password = $('#password').val();
-   // Processing the response
-   $.post("/users",{
-      usrname: user
-   },
-      function(err, res){
-         console.log(err);
-         console.log(res);
-         if(res.body.Username == user){
-            alert("User already exists. Please pick a new username.");
-            console.log("User creation error - user already exists");
-         }
-         else{
-            finishInsertNoUserFound(user,email,password);
-            console.log("Attempting to insert user...");
-         }   
-      }, "json"); 
-}); 
+$(document).ready(function(){
+   $('.btn').click(function(event){
+      event.preventDefault();
+      //External Citation: https://www.w3schools.com/jquery/jquery_dom_set.asp
+      var user = $('#user').val();
+      var email = $('#email').val();
+      var password = $('#password').val();
+      // Processing the response
+      $.post("/users",{
+         usrname: user },
+         function(err, res){
+            console.log(err);
+            console.log(res);
+            if(res.body.Username == user){
+               alert("User already exists. Please pick a new username.");
+               console.log("User creation error - user already exists");
+            }
+            else{
+               console.log("Attempting to insert user...");
+               insertNewUser(user,email,password);
+            }   
+         }, "json"); 
+   }); 
+});
 
-function finishInsertNoUserFound(user, email, password){
+function insertNewUser(user, email, password){
    $.post("/newUser",
          { 
             usrname: user,  
