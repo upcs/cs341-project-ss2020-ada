b40ce71ad88fa6643131361f0fac38e1b40ab2b9
slemly <slemly@outlook.com>
2020-02-26 22:57:05 -0800

    All tests pass. Do not touch.

--- PERU/public/javascripts/index_script.js
+++ PERU/public/javascripts/index_script.js
@@ -1,18 +1,18 @@
 'use strict';
-
-var windowWhoAmI = localStorage.getItem("whoami");
-var currButtonText = "";
-console.log(windowWhoAmI);
-if(windowWhoAmI == 'null'){
-  currButtonText = "Login";
-  $(loginButton).html(currButtonText);
-}
-else if(windowWhoAmI != 'null'){
+window.onload = function(){
+  var windowWhoAmI = localStorage.getItem("whoami");
+  var currButtonText = "";
   console.log(windowWhoAmI);
-  currButtonText = windowWhoAmI + "  logged in. Click to logout.";
-  $(loginButton).html(currButtonText);
+  if(windowWhoAmI == 'null'){
+    currButtonText = "Login";
+    document.getElementById('loginButton').innerHTML = currButtonText;
+  }
+  else if(windowWhoAmI != 'null'){
+    console.log(windowWhoAmI);
+    currButtonText = windowWhoAmI + "  logged in. Click to logout.";
+    document.getElementById('loginButton').innerHTML = currButtonText;
+  }
 }
-
 
 function change_page(value) {
     window.location.href = value;
