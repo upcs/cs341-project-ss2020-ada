2ba5285404925e1fecf5838286709ea7333fab81
slemly <slemly@outlook.com>
2020-02-26 21:03:03 -0800

    Added login/logout functionality

--- PERU/public/javascripts/index_script.js
+++ PERU/public/javascripts/index_script.js
@@ -1,8 +1,22 @@
 'use strict';
 
+var windowWhoAmI = localStorage.getItem("whoami");
+var currButtonText = "";
+console.log(windowWhoAmI);
+if(windowWhoAmI == 'null'){
+  currButtonText = "Login";
+  $(loginButton).html(currButtonText);
+}
+else if(windowWhoAmI != 'null'){
+  console.log(windowWhoAmI);
+  currButtonText = windowWhoAmI + "  logged in. Click to logout.";
+  $(loginButton).html(currButtonText);
+}
+
+
 function change_page(value) {
     window.location.href = value;
   }
- function openNav(){document.getElementById("mySidepanel").style.width = "250px";}
- function closeNav() {document.getElementById("mySidepanel").style.width = "0";}
- module.exports = {openNav, closeNav}
+function openNav(){document.getElementById("mySidepanel").style.width = "250px";}
+function closeNav() {document.getElementById("mySidepanel").style.width = "0";}
+module.exports = {openNav, closeNav}
